3be9ff1c3158dae0f1f6f366193df443a235d5cc
Gabriel <gabrieltohouenou@gmail.com>
2023-11-07 11:42:27 +0100

    first commit

---
+++ src/main/java/example/cashcard/CashCard.java
@@ -0,0 +1,38 @@
+package example.cashcard;
+
+// import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class CashCard {
+
+    @Id
+    private Long id;
+    private Double amount;
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    
+    public CashCard(Long id, Double amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+    
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+}
